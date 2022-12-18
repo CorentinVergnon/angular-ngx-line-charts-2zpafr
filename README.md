014f9fc51967dd29be23da84000687e8a318b60e
CorentinVergnon <corentin.vergnon@gmail.com>
2022-12-19 00:30:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ngx-line-charts-2zpafr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ngx-line-charts-2zpafr)
---
+++ src/app/data.ts
@@ -0,0 +1,65 @@
+export var single = [
+  {
+    "name": "Capteur 1",
+    "series": [
+      {
+        "name": "08:00",
+        "value": "700"
+      },
+      {
+        "name": "09:00",
+        "value": "750"
+      },
+      {
+        "name": "10:00",
+        "value": "1000"
+      },
+      {
+        "name": "11:00",
+        "value": "1200"
+      }
+    ],
+  },
+  {
+    "name": "Capteur 2",
+    "series": [
+      {
+        "name": "08:00",
+        "value": "700"
+      },
+      {
+        "name": "09:00",
+        "value": "720"
+      },
+      {
+        "name": "10:00",
+        "value": "710"
+      },
+      {
+        "name": "11:00",
+        "value": "700"
+      }
+    ],
+  },
+  {
+    "name": "Capteur 3",
+    "series": [
+      {
+        "name": "08:00",
+        "value": "1200"
+      },
+      {
+        "name": "09:00",
+        "value": "1100"
+      },
+      {
+        "name": "10:00",
+        "value": "900"
+      },
+      {
+        "name": "11:00",
+        "value": "800"
+      }
+    ],
+  },
+];
